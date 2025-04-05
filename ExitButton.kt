a02c911b5dce463c66c77b550a7665d717c48cf7
Georgios Takoutis <128981304+GeorgeTak@users.noreply.github.com>
2025-04-06 02:17:43 +0300

    Refactoring_part_2
    Splitting the pause and exit button from the main activity

---
+++ ExitButton.kt
@@ -0,0 +1,73 @@
+package com.example.minesweeper.ui.theme
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import kotlin.system.exitProcess
+
+@Composable
+fun ExitButtonWithConfirmation() {
+    val showDialog = remember { mutableStateOf(false) }
+
+    if (showDialog.value) {
+
+        AlertDialog(
+            onDismissRequest = { showDialog.value = false },
+            title = { Text("Exit") },
+            text = { Text("Are you sure you want to exit the game?") },
+            confirmButton = {
+                Button(
+                    onClick = { exitProcess(0) },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color(0, 0, 139),
+                        contentColor = Color.White,
+                    ),
+                )
+                {
+                    Text("Yes")
+                }
+            },
+            dismissButton = {
+                Button(
+                    onClick = { showDialog.value = false },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color(0, 0, 139),
+                        contentColor = Color.White,
+                    ),
+                ) {
+                    Text("No")
+                }
+            }
+        )
+    }
+
+
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+        Button(
+            onClick = { showDialog.value = true },
+            colors = ButtonDefaults.buttonColors(containerColor = Color(0,0,139), contentColor = Color.White),
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(bottom = 32.dp)
+                .padding(end = 16.dp)
+
+        ) {
+            Text("Exit", fontWeight = FontWeight.Bold)
+        }
+    }
+}
---
+++ PauseButton.kt
@@ -0,0 +1,61 @@
+package com.example.minesweeper.ui.theme
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun PauseResumeButton(isPaused: MutableState<Boolean>) {
+    var showDialog by remember { mutableStateOf(false) }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 1.dp, bottom = 1.dp, start = 4.dp),
+        contentAlignment = Alignment.BottomStart
+    ) {
+        Button(
+            onClick = { showDialog = true },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = if (isPaused.value) Color.Green else Color.Red,
+                contentColor = Color.White
+            ),
+        ) {
+            Text(if (isPaused.value) "Resume" else "Pause")
+        }
+    }
+
+    if (showDialog) {
+        AlertDialog(
+            onDismissRequest = { showDialog = false },
+            title = { Text(if (isPaused.value) "Game Resumed" else "Game Paused") },
+            text = { Text(if (isPaused.value) "The game is now running." else "The game is paused.") },
+            confirmButton = {
+                Button(
+                    onClick = {
+                        isPaused.value = !isPaused.value
+                        showDialog = false
+                    },
+                    colors = ButtonDefaults.buttonColors(containerColor = Color(0, 0, 139), contentColor = Color.White)
+                )
+                {
+                    Text("Got it!")
+                }
+            }
+        )
+    }
+}
